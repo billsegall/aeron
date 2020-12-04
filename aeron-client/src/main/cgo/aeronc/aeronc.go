7d62399d9fbd5f6040d1b02aa26b58daf33d8c6b
billsegall <bill@segall.net>
2020-12-04 13:53:54 +1000

    Round out packed struct tests

--- aeron-client/src/main/cgo/aeronc/aeronc.go
+++ aeron-client/src/main/cgo/aeronc/aeronc.go
@@ -16,7 +16,22 @@ func Setup() {
 	return
 }
 
-func SizeofHeader() uintptr {
+func SizeofHeaderFrame() uintptr {
 	var chvf C.aeron_header_values_frame_t
 	return unsafe.Sizeof(chvf)
+}
+
+func SizeofHeaderValues() uintptr {
+	var chv C.aeron_header_values_t
+	return unsafe.Sizeof(chv)
+}
+
+func SizeofCounterValueDescriptor() uintptr {
+	var ccvd C.aeron_counter_value_descriptor_t
+	return unsafe.Sizeof(ccvd)
+}
+
+func SizeofCounterMetadataDescriptor() uintptr {
+	var ccmd C.aeron_counter_metadata_descriptor_t
+	return unsafe.Sizeof(ccmd)
 }
--- aeron-client/src/main/cgo/aeronc/aeronc_test.go
+++ aeron-client/src/main/cgo/aeronc/aeronc_test.go
@@ -12,9 +12,29 @@ func TestMain(m *testing.M) {
 }
 
 // Given aeron-c uses packed structs make sure we know if anything changes
-func TestSizeof(t *testing.T) {
-	if SizeofHeader() != 32 {
-		t.Log("Unexpected change to header size")
+func TestPackedStructs(t *testing.T) {
+
+	size := SizeofHeaderFrame()
+	if size != 32 {
+		t.Log("Unexpected change to header frame size", size)
+		t.Fail()
+	}
+
+	size = SizeofHeaderValues()
+	if size != 40 {
+		t.Log("Unexpected change to header value size", size)
+		t.Fail()
+	}
+
+	size = SizeofCounterValueDescriptor()
+	if size != 128 {
+		t.Log("Unexpected change to counter value descriptor size", size)
+		t.Fail()
+	}
+
+	size = SizeofCounterMetadataDescriptor()
+	if size != 512 {
+		t.Log("Unexpected change to counter metadata descriptor size", size)
 		t.Fail()
 	}
 }
